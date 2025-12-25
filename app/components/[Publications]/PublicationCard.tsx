84b0e9158bb84be1f2ac12aa0bcbc56b7350011c
Vincent C <vinch.cm@gmail.com>
2025-12-25 09:30:23 +0100

    Change border color depending on dark or light mode

--- app/components/[Publications]/PublicationCard.tsx
+++ app/components/[Publications]/PublicationCard.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export const PublicationCard = (props: ArticleProps) => {
 
   return (
-    <div className="flex flex-col gap-4 shadow-md rounded-lg w-68 h-76 max-sm:w-80 max-sm:h-80 border-purple-500 border-2">
+    <div className="flex flex-col gap-4 shadow-md rounded-lg w-68 h-76 max-sm:w-80 max-sm:h-80 border-purple-500 dark:border-green-500 border-2">
       <img
         src={props.picture}
         alt={props.alt}
@@ -15,7 +15,7 @@ export const PublicationCard = (props: ArticleProps) => {
       <div className="px-4">
         <p className="font-semibold">{props.title}</p>
         <p className="text-sm line-clamp-3">{props.chapeau}</p>
-        <div className="bg-gray-500 text-black text-center w-24 h-8 border-2 border-purple-500 hover:bg-gray-500/60 rounded-lg">
+        <div className="bg-gray-500 text-black text-center w-24 h-8 border-2 dark:border-green-500 border-purple-500 hover:bg-gray-500/60 rounded-lg">
           {props.categoryId == 6 ? (
             <Link href={`/publications/journal/${props.id}`}>Lire</Link>
           ) : (
