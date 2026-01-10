4702ae54e588345887057664ba130e00e921c51e
Vincent C <vinch.cm@gmail.com>
2026-01-10 09:37:07 +0100

    Comments deleted

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -22,63 +22,6 @@ export default function ContactPage() {
             au plus vite !
           </p>
           <Contact />
-          {/* <div className="isolate py-12"> */}
-            {/* <form action="#" method="POST" className="mx-auto max-w-2xl">
-              <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-                {FORM_FIELDS.map((e, index) => (
-                  <FormComponent key={index} {...e} />
-                ))}
-                <div className="sm:col-span-2">
-                  <label
-                    htmlFor="message-subject"
-                    className="block font-semibold"
-                  >
-                    Objet du message
-                  </label>
-                  <div className="relative">
-                    <label htmlFor="subject" className="sr-only">
-                      Sujet
-                    </label>
-                    <select
-                      id="message-subject"
-                      name="message-subject"
-                      className="block w-full text-black rounded-xl border-0 px-3.5 py-2 bg-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-700 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 h-10"
-                      required
-                    >
-                      <option value="">--Choisissez un sujet--</option>
-                      <option value="join">Adhésion</option>
-                      <option value="ad">Encart publicitaire</option>
-                      <option value="event">Evènement</option>
-                      <option value="other">Autre</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="sm:col-span-2">
-                  <label htmlFor="message" className="block font-semibold">
-                    Message
-                  </label>
-                  <div>
-                    <textarea
-                      id="message"
-                      name="message"
-                      rows={4}
-                      className="block w-full rounded-xl border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 bg-gray-200 placeholder:text-gray-700 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
-                      defaultValue={""}
-                    />
-                  </div>
-                </div> */}
-                {/* <PolicyAcceptance /> */}
-              {/* </div> */}
-              {/* <div className="mt-8 flex justify-center">
-                <button
-                  type="submit"
-                  className="bg-blue-500 text-black text-md text-center w-40 max-sm:w-32 max-sm:text-sm py-1 h-8 border-2 border-gray-500 hover:bg-blue-500/60 rounded-lg"
-                >
-                  Envoyer
-                </button>
-              </div> */}
-            {/* </form> */}
-          {/* </div> */}
         </div>
       </section>
     </>
