0db1e4116418d7d5aec6d34510f3ac6329c33341
Vincent C <vinch.cm@gmail.com>
2026-01-10 12:40:36 +0100

    MainLink component created

---
+++ app/components/utils/MainLink.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function MainLink({
+  title,
+  href,
+}: {
+  title: string;
+  href: string;
+}) {
+  return (
+    <Link
+      target="_blank"
+      href={href}
+      className="cta bg-purple-600 hover:bg-purple-600/60"
+    >
+      {title}
+    </Link>
+  );
+}
