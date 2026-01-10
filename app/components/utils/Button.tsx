2fe941fab333f77184d5f02d746de8c6cd69a130
Vincent C <vinch.cm@gmail.com>
2026-01-10 12:26:04 +0100

    Button added in Association page

--- app/components/utils/Button.tsx
+++ app/components/utils/Button.tsx
@@ -1,14 +1,18 @@
+import { MouseEventHandler } from "react";
+
 export default function Button({
   title,
   type,
   style,
+  click,
 }: {
   title: string;
   type: "submit" | "reset" | "button" | undefined;
   style: string;
+  click?: MouseEventHandler<HTMLButtonElement> | undefined;
 }) {
   return (
-    <button type={type} className={style}>
+    <button type={type} className={style} onClick={click}>
       {title}
     </button>
   );
