2adf2f60728465f862ef7b56a38b7b8246c72ccf
Vincent C <vinch.cm@gmail.com>
2026-01-10 18:09:44 +0100

    BackTheJournal done

--- app/components/[Home]/BackTheJournal.tsx
+++ app/components/[Home]/BackTheJournal.tsx
@@ -3,31 +3,27 @@ import MainLink from "../utils/MainLink";
 export const BackTheJournal = () => {
   return (
     <>
-      <div className="mx-auto w-full max-w-7xl p-4 py-6 lg:py-8">
-        <section>
-          <div className="p-4 gap-2">
-            <h2>Soutenir le journal</h2>
-            <p className="">
-              Vous aimez l&apos;initiative et souhaitez investir dans Vues
-              d&apos;Ici ? Vous pouvez choisir d&apos;adhérer à
-              l&apos;association ou faire un don pour financer la prochaine
-              édition, ainsi que rejoindre l&apos;équipe de rédaction !
-            </p>
-            <div className="flex justify-evenly mt-8">
-              <MainLink
-                title={"Nous contacter"}
-                href={"/contact"}
-                style={"cta purpleBG"}
-              />
-              <MainLink
-                title={"Nous soutenir"}
-                href={"/association#nous-soutenir"}
-                style={"cta purpleBG"}
-              />
-            </div>
-          </div>
-        </section>
-      </div>
+      <section className="secondarySection">
+        <h2>Soutenir le journal</h2>
+        <p className="">
+          Vous aimez l&apos;initiative et souhaitez investir dans Vues
+          d&apos;Ici ? Vous pouvez choisir d&apos;adhérer à l&apos;association
+          ou faire un don pour financer la prochaine édition, ainsi que
+          rejoindre l&apos;équipe de rédaction !
+        </p>
+        <div className="flex justify-evenly marginTop">
+          <MainLink
+            title={"Nous contacter"}
+            href={"/contact"}
+            style={"cta purpleBG"}
+          />
+          <MainLink
+            title={"Nous soutenir"}
+            href={"/association#nous-soutenir"}
+            style={"cta purpleBG"}
+          />
+        </div>
+      </section>
     </>
   );
 };
